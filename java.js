8d694e90f1536b0defe9a8a16663c8db5c8dc077
srihas001 <guttulasrihas256@gmail.com>
2025-04-18 12:16:53 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 async function getsongs() {
-    // step1:
-    // we can also create an public API and push our songs into it and follow the below procedure
+    //step1:
+    //we can also create an public API and push our songs into it and follow the below procedure
     // let a= await fetch("http://127.0.0.1:3002/songs/")
     // console.log(a);
     // let response= await a.text()
@@ -8,15 +8,16 @@ async function getsongs() {
     // let div=document.createElement("div")
     // div.innerHTML=response
     //  let tds=div.getElementsByTagName("a")
-    
+     
     //  for (let index = 0; index < tds.length; index++) {
     //     const element = tds[index];
     //     if(element.href.endsWith(".mp3")){
+              
     //         songs.push(element.href.split("/songs/")[1]) 
+            
     //         }
     //     }
-
-    // step2:
+    //step2:
     let songs = [
         "A-Thousand-Years.mp3",
         "Baitikochi-Chuste.mp3",
@@ -28,14 +29,11 @@ async function getsongs() {
         "Perfect-(Mr-Jat.in).mp3",
         "Praanam.mp3",
         "Rao-Gari-Abbayi.mp3"
-    ];
-    return songs;
+    ]
+    return songs
 }
 
 let songs;
-let sri21;
-let currentsong = new Audio();
-
 function formatSeconds(seconds) {
     const totalSeconds = Math.round(seconds);
     const minutes = Math.floor(totalSeconds / 60);
@@ -44,45 +42,36 @@ function formatSeconds(seconds) {
     return `${minutes}:${formattedSeconds}`;
 }
 
-async function main() {
-    sri21 = document.querySelector("#sri21 img");
+let currentsong;
+currentsong = new Audio();
+currentsong.preload = "auto";
 
+async function main() {
     const playback = (track) => {
         currentsong.src = `songs/${track}`;
-        currentsong.load();
-
+        currentsong.preload = "auto";
+        currentsong.play();
+        sri21.src = "images/pause.png";
         document.querySelector(".srihas80").innerHTML = track;
         document.querySelector(".srihas81").innerHTML = "00:00/00:00";
-
-        const playNow = () => {
-            currentsong.play();
-            sri21.src = "images/pause.png";
-            currentsong.removeEventListener("canplaythrough", playNow);
-        };
-
-        currentsong.addEventListener("canplaythrough", playNow);
     }
 
-    songs = await getsongs();
+    let songs = await getsongs();
+    console.log(songs);
     let t = document.querySelector(".sri5").getElementsByTagName("ul")[0];
+    console.log(t);
     for (const song of songs) {
-        t.innerHTML += `
-            <li>
-                <div class="sri10">
-                    <img src="images/musical-note.png" alt="">
-                    <span class="sri12">${song}</span>
-                    <span class="sri11">Play now</span>
-                    <img src="images/play.png" alt="">
-                </div>
-            </li>`;
+        t.innerHTML = t.innerHTML + `<li> <div class="sri10"><img src="images/musical-note.png" alt=""><span class="sri12">${song}</span> <span class="sri11">Play now</span><img src="images/play.png" alt=""></div></li>`;
     }
 
     Array.from(document.querySelector(".sri5").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", () => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".sri12").innerHTML);
             playback(e.querySelector(".sri12").innerHTML.trim());
         });
     });
 
+    let sri21 = document.querySelector("#sri21 img");
     sri21.parentElement.addEventListener("click", () => {
         if (currentsong.paused) {
             currentsong.play();
@@ -101,38 +90,45 @@ async function main() {
     });
 
     document.querySelector(".sri6").addEventListener("click", e => {
+        document.querySelector(".srihas56").style.left = (e.offsetX / e.target.getBoundingClientRect().width) * 100 + "%";
         let pr = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-        document.querySelector(".srihas56").style.left = pr + "%";
-        currentsong.currentTime = (currentsong.duration * pr) / 100;
+        currentsong.currentTime = ((currentsong.duration) * pr) / 100;
     });
 
     let sri22 = document.querySelector("#sri22 img");
     sri22.parentElement.addEventListener("click", () => {
-        let index = songs.indexOf(currentsong.src.split("/").pop());
-        if ((index + 1) < songs.length) playback(songs[index + 1]);
+        let index = songs.indexOf(currentsong.src.split("/").slice(-1)[0]);
+        console.log(index);
+        if ((index + 1) < songs.length)
+            playback(songs[index + 1]);
     });
 
     let sri20 = document.querySelector("#sri20 img");
     sri20.parentElement.addEventListener("click", () => {
-        let index = songs.indexOf(currentsong.src.split("/").pop());
-        if ((index - 1) >= 0) playback(songs[index - 1]);
+        let index = songs.indexOf(currentsong.src.split("/").slice(-1)[0]);
+        console.log(index);
+        if ((index - 1) >= 0)
+            playback(songs[index - 1]);
     });
 
-    document.querySelector(".humburger").addEventListener("click", () => {
+    document.querySelector(".humburger").addEventListener("click", e => {
         document.querySelector(".left").style.left = 0;
     });
 
     document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "-100%";
+        document.querySelector(".left").style.left = -100 + "%";
     });
 
     document.querySelector(".range").addEventListener("change", (e) => {
         currentsong.volume = parseInt(e.target.value) / 100;
     });
 
+    let images = [];
     let y = document.getElementsByClassName("sri2");
     for (let index = 0; index < y.length; index++) {
-        y[index].addEventListener("click", () => {
+        const element = y[index];
+        images.push(element);
+        images[index].addEventListener("click", () => {
             playback(songs[index]);
         });
     }
